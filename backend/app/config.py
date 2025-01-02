eaca0a04417209a6bdf86e7fc340496d11f88782
Michael Bistritzki <wh1ther0seunm4sked@gmail.com>
2025-01-02 23:43:08 +0200

    refactor: remove backend files and update frontend structure

--- backend/app/config.py
+++
@@ -1,6 +0,0 @@
-# backend/app/config.py
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://your_user:your_password@localhost/your_database'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'your_secret_key'
